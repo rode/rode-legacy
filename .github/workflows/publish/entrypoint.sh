7fc3c26eb6fd8afaa0fc4f795e9875dda1d06154
Casey Lee <casey@liatrio.com>
2020-02-13 13:57:41 -0800

    action testing

--- .github/workflows/publish/entrypoint.sh
+++ .github/workflows/publish/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh 
 set -e
 
-SKAFFOLD_DEFAULT_REPO=docker.pkg.github.com/liatrio/rode
-VERSION=$(git describe --tags --dirty | cut -c 2-)
+cd $GITHUB_WORKSPACE
 
+VERSION=$(git describe --tags --dirty | cut -c 2-)
 echo "Publishing $VERSION"
 
-skaffold build
+skaffold build --default-repo docker.pkg.github.com/liatrio/rode
 
 helm lint helm-chart/rode
 helm package --version $VERSION --app-version $VERSION helm-chart/rode
