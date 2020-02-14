7d4fcc6d4917790c477807c06a74c3506a4c8785
Casey Lee <casey@liatrio.com>
2020-02-14 12:32:53 -0800

    github actions to ACR testing

--- .github/workflows/skaffold/entrypoint.sh
+++ .github/workflows/skaffold/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 cd $GITHUB_WORKSPACE
 
-docker login $INPUT_REGISTRY --username $INPUT_USERNAME --password $INPUT_PASSWORD
 echo ${INPUT_USERNAME}
 echo ${INPUT_PASSWORD}
+docker login $INPUT_REGISTRY --username $INPUT_USERNAME --password $INPUT_PASSWORD
 skaffold build --default-repo $INPUT_REGISTRY/rode
