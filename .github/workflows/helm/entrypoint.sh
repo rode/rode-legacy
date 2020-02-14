b54db24f10725fca3bf55e198c09ea7878132574
Casey Lee <casey@liatrio.com>
2020-02-14 11:42:19 -0800

    updates for pipeline

--- .github/workflows/helm/entrypoint.sh
+++ .github/workflows/helm/entrypoint.sh
@@ -7,10 +7,5 @@ VERSION=$(git describe --tags --dirty | cut -c 2-)
 echo "Publishing version '$VERSION'"
 
 helm lint helm-chart/rode
-helm registry login -u $GITHUB_ACTOR -p $INPUT_GITHUB_TOKEN docker.pkg.github.com 
-
-
-sed -i "s/version:.*/version: $VERSION/" helm-chart/rode/Chart.yaml
-sed -i "s/appVersion:.*/appVersion: v$VERSION/" helm-chart/rode/Chart.yaml
-helm chart save helm-chart/rode docker.pkg.github.com/liatrio/rode/chart:$VERSION
-helm chart push docker.pkg.github.com/liatrio/rode/chart:$VERSION
+#helm registry login -u $GITHUB_ACTOR -p $INPUT_GITHUB_TOKEN docker.pkg.github.com 
+helm package --version $VERSION --app-version $VERSION helm-chart/rode
