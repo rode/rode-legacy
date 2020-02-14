1ebbe33393f762d41051ddfa4443bdf5708289de
Casey Lee <casey@liatrio.com>
2020-02-14 15:09:48 -0800

    github actions to ACR testing

--- .github/workflows/helm/entrypoint.sh
+++ .github/workflows/helm/entrypoint.sh
@@ -7,6 +7,8 @@ VERSION=$(git describe --tags --dirty | cut -c 2-)
 
 sed -i "s/appVersion:.*/appVersion: v${VERSION}/" helm-chart/rode/Chart.yaml
 helm lint helm-chart/rode
+
+helm plugin install https://github.com/chartmuseum/helm-push
 helm plugin list
 helm push -h
 helm push helm-chart/rode $INPUT_REPOSITORY -u $INPUT_USERNAME -p $INPUT_PASSWORD -v $VERSION
