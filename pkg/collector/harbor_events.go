0e2400657ea927db6009d6d9f03655b3db0fad1e
Takumi Nishida <takumin@liatrio.com>
2020-02-17 15:10:33 -0800

    implement delete for webhook policies

--- pkg/collector/harbor_events.go
+++ pkg/collector/harbor_events.go
@@ -4,16 +4,17 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"github.com/go-logr/logr"
-	"github.com/liatrio/rode/pkg/occurrence"
 	"io/ioutil"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
+
+	"github.com/go-logr/logr"
+	"github.com/liatrio/rode/pkg/occurrence"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 type HarborEventCollector struct {
@@ -23,24 +24,6 @@ type HarborEventCollector struct {
 	secret            string
 	project           string
 	namespace         string
-}
-
-type Project struct {
-	ProjectID int    `json:"project_id"`
-	Name      string `json:"name"`
-}
-
-type JsonInput struct {
-	Targets    []Targets `json:"targets"`
-	EventTypes []string  `json:"event_types"`
-	Enabled    bool      `json:"enabled"`
-}
-
-type Targets struct {
-	Type           string `json:"type"`
-	Address        string `json:"address"`
-	AuthHeader     string `json:"auth_header"`
-	SkipCertVerify bool   `json:"skip_cert_verify"`
 }
 
 func NewHarborEventCollector(logger logr.Logger, harborUrl string, secret string, project string, namespace string) Collector {
@@ -54,6 +37,7 @@ func NewHarborEventCollector(logger logr.Logger, harborUrl string, secret string
 }
 
 func (t *HarborEventCollector) Reconcile(ctx context.Context) error {
+
 	t.logger.Info("reconciling HARBOR collector")
 	harborCreds := t.getHarborCredentials(ctx, t.secret, t.namespace)
 	projectID := t.getProjectID(t.project, t.url)
@@ -84,12 +68,15 @@ func (t *HarborEventCollector) Start(ctx context.Context, stopChan chan interfac
 
 func (t *HarborEventCollector) Destroy(ctx context.Context) error {
 	t.logger.Info("destroying test collector")
+	harborCreds := t.getHarborCredentials(ctx, t.secret, t.namespace)
+	projectID := t.getProjectID(t.project, t.url)
+	policyID := t.getWebhookPolicyID(projectID, t.url, harborCreds)
+	t.deleteWebhookPolicy(projectID, t.url, policyID, harborCreds)
 
 	return nil
 }
 
 func (t *HarborEventCollector) getHarborCredentials(ctx context.Context, secretname string, namespace string) string {
-	t.logger.Info("Inside getHarborCredentials\n")
 	config, configError := rest.InClusterConfig()
 	if configError != nil {
 		log.Fatal(configError)
@@ -123,19 +110,37 @@ func (t *HarborEventCollector) getProjectID(name string, url string) string {
 
 	json.Unmarshal([]byte(projectList), &projects)
 
-	projectId := ""
+	projectID := ""
 	for _, p := range projects {
 		if p.Name == name {
-			projectId = strconv.Itoa(p.ProjectID)
+			projectID = strconv.Itoa(p.ProjectID)
 		}
 	}
-	return projectId
+	return projectID
 }
 
-func (t *HarborEventCollector) checkForWebhook(projectId string, url string, harborCreds string) bool {
+func (t *HarborEventCollector) getWebhookPolicyID(projectID string, url string, harborCreds string) string {
 	client := &http.Client{}
+	webhookPolicyIDURL := url + "/api/projects/" + projectID + "/webhook/policies"
+	req, err := http.NewRequest("GET", webhookPolicyIDURL, nil)
+	req.SetBasicAuth("admin", harborCreds)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Print(err)
+	}
+	defer resp.Body.Close()
+	policyList, err := ioutil.ReadAll(resp.Body)
 
-	webhookURL := url + "/api/projects/" + projectId + "/webhook/policies"
+	var policies []WebhookPolicies
+	json.Unmarshal([]byte(policyList), &policies)
+	policyID := strconv.Itoa(policies[0].ID)
+
+	return policyID
+}
+
+func (t *HarborEventCollector) checkForWebhook(projectID string, url string, harborCreds string) bool {
+	client := &http.Client{}
+	webhookURL := url + "/api/projects/" + projectID + "/webhook/policies"
 
 	req, err := http.NewRequest("GET", webhookURL, nil)
 	req.SetBasicAuth("admin", harborCreds)
@@ -156,12 +161,11 @@ func (t *HarborEventCollector) checkForWebhook(projectId string, url string, har
 	return true
 }
 
-func (t *HarborEventCollector) createWebhook(projectId string, url string, harborCreds string, webhookEndpoint string) {
+func (t *HarborEventCollector) createWebhook(projectID string, url string, harborCreds string, webhookEndpoint string) {
 	client := &http.Client{}
 
-	webhookURL := url + "/api/projects/" + projectId + "/webhook/policies"
-
-	bodyTargets := []Targets{
+	webhookURL := url + "/api/projects/" + projectID + "/webhook/policies"
+	targets := []Targets{
 		Targets{
 			Type:           "http",
 			Address:        webhookEndpoint,
@@ -169,9 +173,8 @@ func (t *HarborEventCollector) createWebhook(projectId string, url string, harbo
 			SkipCertVerify: true,
 		},
 	}
-
-	body := &JsonInput{
-		Targets: bodyTargets,
+	body := &WebhookPolicies{
+		Targets: targets,
 		EventTypes: []string{
 			"pushImage",
 			"scanningFailed",
@@ -181,14 +184,50 @@ func (t *HarborEventCollector) createWebhook(projectId string, url string, harbo
 	}
 
 	bodyJson, err := json.Marshal(body)
-	req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(bodyJson))
 
+	req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(bodyJson))
 	req.SetBasicAuth("admin", harborCreds)
 
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Print(err)
+		return
 	}
 	defer resp.Body.Close()
 
+	log.Print("Successfully created webhook.")
+}
+
+func (t *HarborEventCollector) deleteWebhookPolicy(projectID string, url string, policyID string, harborCreds string) {
+	client := &http.Client{}
+
+	webhookDeleteURL := url + "/api/projects/" + projectID + "/webhook/policies" + policyID
+	req, err := http.NewRequest("DELETE", webhookDeleteURL, nil)
+	_, err = client.Do(req)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	log.Print("Successfully deleted webhook.")
+}
+
+// Harbor structured project
+type Project struct {
+	ProjectID int    `json:"project_id"`
+	Name      string `json:"name"`
+}
+
+// Harbor structured project
+type WebhookPolicies struct {
+	Targets    []Targets `json:"targets,omitempty"`
+	EventTypes []string  `json:"event_types,omitempty"`
+	Enabled    bool      `json:"enabled,omitempty"`
+	ID         int       `json:"id,omitempty"`
+}
+
+type Targets struct {
+	Type           string `json:"type"`
+	Address        string `json:"address"`
+	AuthHeader     string `json:"auth_header"`
+	SkipCertVerify bool   `json:"skip_cert_verify"`
 }
