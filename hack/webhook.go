bcd9165f52925c224e1b9abd79e60db35605b950
Jordan A <jordana@Jordans-Liatrio-Macbook-Pro.local>
2020-02-17 11:14:44 -0800

    implement webhook creation

--- hack/webhook.go
+++ hack/webhook.go
@@ -1,116 +1,110 @@
 package main
 
 import (
-  "strconv"
-  "net/http"
-  "log"
-  "bytes"
-  "encoding/json"
-  "io/ioutil"
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 type Project struct {
-	ProjectID          int       `json:"project_id"`
-	Name               string    `json:"name"`
+	ProjectID int    `json:"project_id"`
+	Name      string `json:"name"`
 }
 
 type JsonInput struct {
 	Targets    []Targets `json:"targets"`
-	EventTypes []string `json:"event_types"`
-  Enabled    bool `json:"enabled"`
+	EventTypes []string  `json:"event_types"`
+	Enabled    bool      `json:"enabled"`
 }
 
 type Targets struct {
 	Type           string `json:"type"`
-  Address        string `json:"address"`
-  AuthHeader     string `json:"auth_header"`
-  SkipCertVerify bool   `json:"skip_cert_verify"`
+	Address        string `json:"address"`
+	AuthHeader     string `json:"auth_header"`
+	SkipCertVerify bool   `json:"skip_cert_verify"`
 }
 
 func main() {
 
-  client := &http.Client{
-  }
+	client := &http.Client{}
 
-  address := "http://5dc2f76a.ngrok.io/webhook"
-  name := "webhook-test"
-  projectId := ""
+	address := "http://5dc2f76a.ngrok.io/webhook"
+	name := "webhook-test"
+	projectId := ""
 
-  bodyTargets := []Targets {
-    Targets{
-      Type: "http",
-      Address: address,
-      AuthHeader: "auth_header",
-      SkipCertVerify: true,
-    },
-  }
+	bodyTargets := []Targets{
+		Targets{
+			Type:           "http",
+			Address:        address,
+			AuthHeader:     "auth_header",
+			SkipCertVerify: true,
+		},
+	}
 
-  body := &JsonInput{
-    Targets: bodyTargets,
-    EventTypes: []string {
-      "pushImage",
-      "scanningFailed",
-      "scanningCompleted",
-    },
-    Enabled: true,
-  }
+	body := &JsonInput{
+		Targets: bodyTargets,
+		EventTypes: []string{
+			"pushImage",
+			"scanningFailed",
+			"scanningCompleted",
+		},
+		Enabled: true,
+	}
 
+	bodyJson, err := json.Marshal(body)
 
-  bodyJson, err := json.Marshal(body)
+	request, err := http.NewRequest("GET", "https://harbor.toolchain.lead.sandbox.liatr.io/api/projects/", nil)
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Print(err)
+	}
 
-  request, err := http.NewRequest("GET", "https://harbor.toolchain.lead.sandbox.liatr.io/api/projects/", nil)
-  resp, err := client.Do(request)
-  if err != nil {
-    log.Print(err)
-  }
+	projectList, err := ioutil.ReadAll(resp.Body)
 
-  projectList, err := ioutil.ReadAll(resp.Body)
+	var project []Project
 
-  var project []Project
+	json.Unmarshal([]byte(projectList), &project)
 
-  json.Unmarshal([]byte(projectList), &project)
+	for _, p := range project {
+		if p.Name == name {
+			projectId = strconv.Itoa(p.ProjectID)
+		}
+	}
 
-  for _, p := range project {
-    if p.Name == name {
-      projectId = strconv.Itoa(p.ProjectID)
-    }
-  }
+	if projectId != "" {
 
-  if projectId != ""{
+		url := "https://harbor.toolchain.lead.sandbox.liatr.io/api/projects/" + projectId + "/webhook/policies"
 
+		request, err := http.NewRequest("GET", url, nil)
+		request.SetBasicAuth("", "")
+		resp, err := client.Do(request)
+		if err != nil {
+			log.Print(err)
+		}
 
-    url := "https://harbor.toolchain.lead.sandbox.liatr.io/api/projects/" + projectId + "/webhook/policies"
+		//log.Print(resp)
 
-    request, err := http.NewRequest("GET", url, nil)
-    request.SetBasicAuth("", "")
-    resp, err := client.Do(request)
-    if err != nil {
-      log.Print(err)
-    }
+		var webhooks []string
+		webhookJson, err := ioutil.ReadAll(resp.Body)
+		json.Unmarshal([]byte(webhookJson), &webhooks)
 
+		if len(webhooks) == 0 {
 
+			req, err := http.NewRequest("POST", url, bytes.NewBuffer(bodyJson))
 
-    //log.Print(resp)
+			req.SetBasicAuth("", "")
+			//req.Header.Add("If-None-Match", `W/"wyzzy"`)
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Print(err)
+			}
+			log.Print(resp)
 
-    var webhooks []string
-    webhookJson, err := ioutil.ReadAll(resp.Body)
-    json.Unmarshal([]byte(webhookJson), &webhooks)
+			defer resp.Body.Close()
 
-    if len(webhooks) == 0 {
- 
-
-      req, err := http.NewRequest("POST", url, bytes.NewBuffer(bodyJson))
-
-      req.SetBasicAuth("", "")
-      //req.Header.Add("If-None-Match", `W/"wyzzy"`)
-      resp, err := client.Do(req)
-      if err != nil {
-        log.Print(err)
-      }
-      log.Print(resp)
-      
-      defer resp.Body.Close()
-
-    }
-  }
+		}
+	}
 }
--- pkg/collector/harbor_events.go
+++ pkg/collector/harbor_events.go
@@ -1,19 +1,19 @@
 package collector
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"github.com/go-logr/logr"
 	"github.com/liatrio/rode/pkg/occurrence"
+	"io/ioutil"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"log"
-	"time"
-	"strconv"
 	"net/http"
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
+	"strconv"
+	"time"
 )
 
 type HarborEventCollector struct {
@@ -22,40 +22,44 @@ type HarborEventCollector struct {
 	url               string
 	secret            string
 	project           string
+	namespace         string
 }
 
 type Project struct {
-	ProjectID          int       `json:"project_id"`
-	Name               string    `json:"name"`
+	ProjectID int    `json:"project_id"`
+	Name      string `json:"name"`
 }
 
 type JsonInput struct {
 	Targets    []Targets `json:"targets"`
-	EventTypes []string `json:"event_types"`
-  Enabled    bool `json:"enabled"`
+	EventTypes []string  `json:"event_types"`
+	Enabled    bool      `json:"enabled"`
 }
 
 type Targets struct {
 	Type           string `json:"type"`
-  Address        string `json:"address"`
-  AuthHeader     string `json:"auth_header"`
-  SkipCertVerify bool   `json:"skip_cert_verify"`
+	Address        string `json:"address"`
+	AuthHeader     string `json:"auth_header"`
+	SkipCertVerify bool   `json:"skip_cert_verify"`
 }
 
-func NewHarborEventCollector(logger logr.Logger, harborUrl string, secret string, project string) Collector {
+func NewHarborEventCollector(logger logr.Logger, harborUrl string, secret string, project string, namespace string) Collector {
 	return &HarborEventCollector{
-		logger:  logger,
-		url:     harborUrl,
-		secret:  secret,
-		project: project,
+		logger:    logger,
+		url:       harborUrl,
+		secret:    secret,
+		project:   project,
+		namespace: namespace,
 	}
 }
 
 func (t *HarborEventCollector) Reconcile(ctx context.Context) error {
 	t.logger.Info("reconciling HARBOR collector")
-	t.getHarborCredentials(ctx, t.secret)
-	//checkForWebhook
-	//If webhook doesn't exist, createWebhook
+	harborCreds := t.getHarborCredentials(ctx, t.secret, t.namespace)
+	projectID := t.getProjectID(t.project, t.url)
+	if projectID != "" && !t.checkForWebhook(projectID, t.url, harborCreds) {
+		t.createWebhook(projectID, t.url, harborCreds, "www.example.com")
+	}
 
 	return nil
 }
@@ -84,7 +88,7 @@ func (t *HarborEventCollector) Destroy(ctx context.Context) error {
 	return nil
 }
 
-func (t *HarborEventCollector) getHarborCredentials(ctx context.Context, secretname string) {
+func (t *HarborEventCollector) getHarborCredentials(ctx context.Context, secretname string, namespace string) string {
 	t.logger.Info("Inside getHarborCredentials\n")
 	config, configError := rest.InClusterConfig()
 	if configError != nil {
@@ -96,11 +100,95 @@ func (t *HarborEventCollector) getHarborCredentials(ctx context.Context, secretn
 		log.Fatal(clientErr)
 	}
 
-	secrets, err := clientset.CoreV1().Secrets("rode").Get(secretname, metav1.GetOptions{})
+	secrets, err := clientset.CoreV1().Secrets(namespace).Get(secretname, metav1.GetOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// t.logger.Info("Number of secrets", "numSec", len(secrets.Items))
-	t.logger.Info("DATA", "data", secrets.Data["HARBOR_ADMIN_PASSWORD"])
+	return string(secrets.Data["HARBOR_ADMIN_PASSWORD"])
+}
+
+func (t *HarborEventCollector) getProjectID(name string, url string) string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url+"/api/projects/", nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Print(err)
+	}
+	defer resp.Body.Close()
+
+	projectList, err := ioutil.ReadAll(resp.Body)
+
+	var projects []Project
+
+	json.Unmarshal([]byte(projectList), &projects)
+
+	projectId := ""
+	for _, p := range projects {
+		if p.Name == name {
+			projectId = strconv.Itoa(p.ProjectID)
+		}
+	}
+	return projectId
+}
+
+func (t *HarborEventCollector) checkForWebhook(projectId string, url string, harborCreds string) bool {
+	client := &http.Client{}
+
+	webhookURL := url + "/api/projects/" + projectId + "/webhook/policies"
+
+	req, err := http.NewRequest("GET", webhookURL, nil)
+	req.SetBasicAuth("admin", harborCreds)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Print(err)
+	}
+
+	defer resp.Body.Close()
+
+	var webhooks []string
+	webhookJson, err := ioutil.ReadAll(resp.Body)
+	json.Unmarshal([]byte(webhookJson), &webhooks)
+
+	if len(webhooks) == 0 {
+		return false
+	}
+	return true
+}
+
+func (t *HarborEventCollector) createWebhook(projectId string, url string, harborCreds string, webhookEndpoint string) {
+	client := &http.Client{}
+
+	webhookURL := url + "/api/projects/" + projectId + "/webhook/policies"
+
+	bodyTargets := []Targets{
+		Targets{
+			Type:           "http",
+			Address:        webhookEndpoint,
+			AuthHeader:     "auth_header",
+			SkipCertVerify: true,
+		},
+	}
+
+	body := &JsonInput{
+		Targets: bodyTargets,
+		EventTypes: []string{
+			"pushImage",
+			"scanningFailed",
+			"scanningCompleted",
+		},
+		Enabled: true,
+	}
+
+	bodyJson, err := json.Marshal(body)
+	req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(bodyJson))
+
+	req.SetBasicAuth("admin", harborCreds)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Print(err)
+	}
+	defer resp.Body.Close()
+
 }
